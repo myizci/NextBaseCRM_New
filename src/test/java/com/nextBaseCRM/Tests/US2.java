4d35b3acdcdd348ee5a58b1c95073078842ded93
myizci <izci@hotmail.com>
2021-02-03 11:54:19 -0500

    02/03/2021  commit#5

--- src/test/java/com/nextBaseCRM/Tests/US2.java
+++ src/test/java/com/nextBaseCRM/Tests/US2.java
@@ -3,8 +3,11 @@ package com.nextBaseCRM.Tests;
 import com.nextBaseCRM.Pages.MainPage;
 import com.nextBaseCRM.Utils.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class US2 extends TestBase {
 
@@ -28,5 +31,37 @@ public class US2 extends TestBase {
 
         Assert.assertEquals(actualMessage, message);
         Driver.getDriver().switchTo().parentFrame();// getting out iframe
+
+
+        /*
+        Another approach
+
+        Driver.getDriver().findElement(By.xpath(MainPage.sendMessageBoxXpath)).click();
+        WebElement frame = Driver.getDriver().findElement(By.xpath("//iframe[@class='bx-editor-iframe']"));
+        Driver.getDriver().switchTo().frame(frame);
+       Driver.getDriver().findElement(By.xpath("//body[@contenteditable='true']")).sendKeys("Hello8");
+        Driver.getDriver().switchTo().defaultContent();
+        Driver.getDriver().findElement(By.xpath(MainPage.sendButton)).click();
+        // verify
+
+        List<WebElement> list = Driver.getDriver().findElements(By.xpath("//div[@class='feed-post-text-block-inner-inner']"));
+        boolean testFlag = true;
+        for (WebElement webElement : list) {
+            if (webElement.getText().equals("Hello8")) {
+                System.out.println("Text displayed, PASS");
+                testFlag = false;
+                //break;
+            }
+        }
+
+        if (testFlag) {
+            System.out.println("Test Failed!");
+        }
+         */
+    }
+
+    @Test
+    public void US2_TC2_userCancelMessage() {
+
     }
 }
