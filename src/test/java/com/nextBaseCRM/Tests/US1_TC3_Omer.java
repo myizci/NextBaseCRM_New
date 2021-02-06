f96f9117c9e251ab3ad3cc30cd0a42c5fb90f475
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2021-02-06 13:39:13 -0500

    US12Page is added into the page folder and US12_Ghalip is improved and tested. Requested to merge into the master branch. New branch added.

---
+++ src/test/java/com/nextBaseCRM/Tests/US1_TC3_Omer.java
@@ -0,0 +1,24 @@
+package com.nextBaseCRM.Tests;
+
+import com.nextBaseCRM.Pages.LoginPage;
+import com.nextBaseCRM.Utils.ConfigurationReader;
+import com.nextBaseCRM.Utils.Driver;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US1_TC3_Omer {
+
+    @Test
+    public void US1_TC3_userCanAccessForgotPassword() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        Driver.getDriver().findElement(By.xpath(LoginPage.forgotPasswordLink)).click();
+        String expectedForgotPasswordTitle = "Get Password";
+        String actualForgotPasswordTitle = Driver.getDriver().getTitle();
+        System.out.println("actualForgotPasswordTitle = " + actualForgotPasswordTitle);
+        Assert.assertEquals(expectedForgotPasswordTitle,actualForgotPasswordTitle,"Cannot reach forgot password link, FAILED");
+
+
+    }
+
+}
